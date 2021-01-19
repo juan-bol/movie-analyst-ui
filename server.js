a9862abfdfdcd391eb4b4349cd0b8dab3b5eccf0
juan-bol <juan-bol98@hotmail.com>
2021-01-19 17:19:55 -0500

    backend url changed

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Declare our dependencies
 var express = require('express');
 var request = require('superagent');
-var backendHost = process.env.BACK_HOST || '192.158.1.1'; // TODO, temporaly burned
+// var backendHost = process.env.BACK_HOST || '192.158.1.1'; // TODO, temporaly burned
+var backendHost = '192.158.1.1'; // TODO, temporaly burned
 // Create our express app
 var app = express();
 
